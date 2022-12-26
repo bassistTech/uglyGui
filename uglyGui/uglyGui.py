71bb13ef97d4b622012fad62dcac6697e1e115f8
Francis <hpf.tech.llc@gmail.com>
2022-12-26 16:54:20 -0600

    minor feature additions

--- uglyGui/uglyGui.py
+++ uglyGui/uglyGui.py
@@ -4,51 +4,56 @@ uglyGui
 MIT License
 '''
 
+import pandastable
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
 import tkinter as tk
 import matplotlib
 matplotlib.use("TkAgg")
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
+
 
 class MainDialog(tk.Tk):
     def __init__(self, title):
         super().__init__()
         self.wm_title(title)
         self.protocol("WM_DELETE_WINDOW", self.allQuit)
-        
+
     def allQuit(self):
         self.destroy()
         self.quit()
-        
+
     def show(self):
         self.mainloop()
 
+
 class SubDialog(tk.Toplevel):
-    def __init(self, title):
-        super().__init()
+    def __init__(self, title):
+        super().__init__()
         self.wm_title(title)
         self.withdraw()
         self.protocol("WM_DELETE_WINDOW", self.withdraw)
-        
+
     def show(self):
         self.deiconify()
 
+
 class Button(tk.Button):
-    def __init__(self, parent, label, action = None):
-        frame = tk.Frame(master = parent)
-        frame.pack(side = tk.TOP, fill = tk.BOTH)
-        super().__init__(text = label, command = action)
+    def __init__(self, parent, label, action=None):
+        frame = tk.Frame(master=parent)
+        frame.pack(side=tk.TOP, fill=tk.BOTH)
+        super().__init__(text=label, command=action)
         self.pack()
+
 
 class __Entry(tk.Entry):
     def __init__(self, parent, label, init, datatype):
 
-        frame = tk.Frame(master = parent)
-        frame.pack(side = tk.TOP, fill = tk.BOTH)
-        
-        tk.Label(frame, text=label).pack(side = tk.LEFT)
+        frame = tk.Frame(master=parent)
+        frame.pack(side=tk.TOP, fill=tk.BOTH)
+
+        tk.Label(frame, text=label).pack(side=tk.LEFT)
         self.value = tk.StringVar()
-        super().__init__(frame, textvariable = self.value, width = 15)
-        self.pack(side = tk.RIGHT, fill = tk.X)
+        super().__init__(frame, textvariable=self.value, width=15)
+        self.pack(side=tk.RIGHT, fill=tk.X)
         self.value.set(str(init))
         self.datatype = datatype
 
@@ -58,17 +63,21 @@ class __Entry(tk.Entry):
     def upd(self, s):
         self.value.set(str(s))
 
+
 class IntEntry(__Entry):
     def __init__(self, parent, label, init):
         super().__init__(parent, label, init, 'I')
-        
+
+
 class FloatEntry(__Entry):
     def __init__(self, parent, label, init):
         super().__init__(parent, label, init, 'F')
-        
+
+
 class TextEntry(__Entry):
     def __init__(self, parent, label, init):
         super().__init__(parent, label, init, 'S')
+
 
 class RadioButtons():
     def __init__(self,
@@ -76,34 +85,35 @@ class RadioButtons():
 
         self.value = tk.IntVar()
         self.value.set(init)
-        frame = tk.Frame(master = parent)
-        frame.pack(side = tk.TOP)
+        frame = tk.Frame(master=parent)
+        frame.pack(side=tk.TOP)
         if vertical:
             ps = tk.TOP
         else:
             ps = tk.LEFT
-        tk.Label(frame, text = label).pack(side=ps)
+        tk.Label(frame, text=label).pack(side=ps)
         for i in range(len(buttonLabels)):
-            tk.Radiobutton(frame, text = buttonLabels[i], value = i,
-                               variable = self.value).pack(side=ps)
+            tk.Radiobutton(frame, text=buttonLabels[i], value=i,
+                           variable=self.value).pack(side=ps)
 
     def get(self):
         return self.value.get()
 
     def upd(self, i):
         self.value.set(i)
+
 
 class CheckBox(tk.Checkbutton):
     def __init__(self, parent, label, init, callback):
         self.value = tk.IntVar()
         self.value.set(init)
-        frame = tk.Frame(master = parent)
-        frame.pack(side = tk.TOP, fill = tk.BOTH)
+        frame = tk.Frame(master=parent)
+        frame.pack(side=tk.TOP, fill=tk.BOTH)
         super().__init__(frame,
-                       text = label,
-                       command = callback,
-                       variable = self.value)
-        self.pack(side = tk.LEFT)
+                         text=label,
+                         command=callback,
+                         variable=self.value)
+        self.pack(side=tk.LEFT)
 
     def get(self):
         return self.value.get()
@@ -111,18 +121,23 @@ class CheckBox(tk.Checkbutton):
     def upd(self, i):
         self.value.set(i)
 
+
 class Graph():
     def __init__(self, parent, r, c):
         self.fig = matplotlib.figure.Figure()
-        self.axes = [self.fig.add_subplot(s) 
+        self.axes = [self.fig.add_subplot(s)
                      for s in matplotlib.gridspec.GridSpec(r, c)]
         self.canvas = FigureCanvasTkAgg(self.fig, master=parent)
         self.canvas.draw()
-        self.canvas.get_tk_widget().pack(side = tk.TOP, fill=tk.BOTH, expand=1)
+        self.canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
         toolbar = NavigationToolbar2Tk(self.canvas, parent)
         toolbar.update()
-        toolbar.pack(side = tk.TOP)
-        
+        toolbar.pack(side=tk.TOP)
+
+    def update(self):
+        self.canvas.draw()
+
+
 class TextEditor():
     '''
     I adopted the code from:
@@ -130,32 +145,45 @@ class TextEditor():
     The only nuance is that I put the text editor in a frame, so it can coexist
     with other widgets in a dialog
     '''
+
     def __init__(self, parent, init):
-        #self.value = tk.StringVar
-        frame = tk.Frame(master = parent)
-        frame.pack(side = tk.TOP, fill = tk.BOTH, expand = 1)
+        frame = tk.Frame(master=parent)
+        frame.pack(side=tk.TOP, fill=tk.BOTH, expand=1)
         self.scrollbar = tk.Scrollbar(frame)
         self.scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
         self.text = tk.Text(frame,
-                            width = 50, height = 20, 
+                            width=50, height=20,
                             yscrollcommand=self.scrollbar.set)
-        self.text.pack(fill=tk.BOTH, expand = 1)
+        self.text.pack(fill=tk.BOTH, expand=1)
         self.scrollbar.config(command=self.text.yview)
         self.text.insert('1.0', init)
-        
+
     def get(self):
         return self.text.get('1.0', 'end')
-    
+
     def upd(self, text):
         self.text.delete('1.0', 'end')
         self.text.insert('1.0', text)
-        
+
+
+class TableEdit():
+    def __init__(self, parent, df):
+        frame = tk.Frame(master=parent)
+        frame.pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+        self.df = df
+        self.table = pandastable.Table(frame, dataframe=self.df,
+                                       showtoolbar=True, showstatusbar=True)
+        self.table.showIndex()
+        self.table.show()
+
+
 if __name__ == '__main__':
     def goBu():
         print(te.get())
         te.upd('Mobbs')
-        
+
     mainDialog = MainDialog('Text editor test')
     te = TextEditor(mainDialog, 'hee')
-    bu = Button(mainDialog, 'Do something', action = goBu)
+    bu = Button(mainDialog, 'Do something', action=goBu)
+    subDialog = SubDialog()
     mainDialog.show()
